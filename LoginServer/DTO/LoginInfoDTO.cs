6bb18d1373129719f0f5e4f426a339490b559fed
s@d.com <s@d.com>
2024-05-20 13:12:58 +0300

    t

---
+++ LoginServer/DTO/LoginInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginServer.DTO
+{
+    public class LoginInfoDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
--- LoginServer/ModelsEXT/LoginDemoDbContext.cs
+++ LoginServer/ModelsEXT/LoginDemoDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LoginServer.DTO;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 namespace LoginServer.Models
@@ -11,9 +12,14 @@ namespace LoginServer.Models
             return this.Users.Where(user => user.Email == email).FirstOrDefault(); 
         }
 
-        public User Login(string email, string pass)
+        //public User Login(string email, string pass)
+        //{
+        //    return this.Users.Where(user => user.Email == email && user.Password == pass).FirstOrDefault();
+        //}
+
+        public User Login(LoginInfoDTO loginDTO)
         {
-            return this.Users.Where(user => user.Email == email && user.Password == pass).FirstOrDefault();
+            return this.Users.Where(user => user.Email == loginDTO.Email && user.Password == loginDTO.Password).FirstOrDefault();
         }
 
     }
